6f794dd933af1fd21ad46d095b10152bba733204
tony <chcjswo@gmail.com>
2021-05-24 01:55:52 +0900

    Querydsl 페이징 테스트

--- src/main/java/com/mocadev/querydsl/controller/MemberController.java
+++ src/main/java/com/mocadev/querydsl/controller/MemberController.java
@@ -3,8 +3,11 @@ package com.mocadev.querydsl.controller;
 import com.mocadev.querydsl.dto.MemberSearchCondition;
 import com.mocadev.querydsl.dto.MemberTeamDto;
 import com.mocadev.querydsl.repository.MemberJpaRepository;
+import com.mocadev.querydsl.repository.MemberRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,10 +23,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
 	private final MemberJpaRepository memberJpaRepository;
+	private final MemberRepository memberRepository;
 
 	@GetMapping("/v1/members")
 	public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
 		return memberJpaRepository.searchByWhere(condition);
+	}
+
+	@GetMapping("/v2/members")
+	public Page<MemberTeamDto> searchMemberV2(MemberSearchCondition condition, Pageable pageable) {
+		return memberRepository.searchPageSimple(condition, pageable);
+	}
+
+	@GetMapping("/v3/members")
+	public Page<MemberTeamDto> searchMemberV3(MemberSearchCondition condition, Pageable pageable) {
+		return memberRepository.searchPageComplex(condition, pageable);
 	}
 
 }
