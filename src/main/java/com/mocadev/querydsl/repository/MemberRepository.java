6fa25aa60791e6b71baa0a424ad6878f22d2d044
tony <chcjswo@gmail.com>
2021-05-26 01:40:13 +0900

    QuerydslPredicateExecutor

--- src/main/java/com/mocadev/querydsl/repository/MemberRepository.java
+++ src/main/java/com/mocadev/querydsl/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package com.mocadev.querydsl.repository;
 import com.mocadev.querydsl.entity.Member;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 /**
  * @author chcjswo
@@ -11,7 +12,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @github http://github.com/chcjswo
  * @since 2021-05-24
  **/
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom,
+	QuerydslPredicateExecutor<Member> {
 
 	List<Member> findByUsername(String username);
 
